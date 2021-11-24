b588545bf1bb168eb0853ae36525d5407657eb7b
Salil Mehta <salil.mehta@huawei.com>
2021-11-24 16:09:08 +0800

    arm/cpuhp: Changes to pre-size GIC with possible vcpus @machine init
    GIC needs to be pre-sized with possible vcpus at the initialization time. This
    is necessary because Memory regions and resources associated with GICC/GICR
    etc cannot be changed (add/del/modified) after VM has inited. Also, GIC_TYPER
    needs to be initialized with mp_affinity and cpu interface number association.
    This cannot be changed after GIC has initialized.
    
    Once all the cpu interfaces of the GIC has been inited it needs to be ensured
    that any updations to the GICC during reset only takes place for the present
    vcpus and not the disabled ones. Therefore, proper checks are required at
    various places.
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -187,7 +187,7 @@ static inline int virt_gicv3_redist_region_count(VirtMachineState *vms)
 
     assert(vms->gic_version == VIRT_GIC_VERSION_3);
 
-    return MACHINE(vms)->smp.cpus > redist0_capacity ? 2 : 1;
+    return vms->max_cpus > redist0_capacity ? 2 : 1;
 }
 
 #endif /* QEMU_ARM_VIRT_H */
